5bdffb692c61f46b50f0d331f95dd6744e6fa221
jeffersontk <jefferson.britotk@gmail.com>
2019-10-20 00:53:47 -0300

    adicionando validação de campos do formulario

--- index.php
+++ index.php
@@ -1,34 +1,21 @@
-<?php
-    $categorias = [];
-    $categorias[] = 'infantil';
-    $categorias[] = 'adolescente';
-    $categorias[] = 'adulto';
-    $categorias[] = 'idoso';
-    print_r($categorias);
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Formulario de inscrição</title>
+</head>
+<body>
 
-    $nome = 'Jeff';
-    $idade = 8;
-    
-    var_dump($nome);
-    var_dump($idade);
+    <h2>Formulario de Inscrição</h2>
 
-    if($idade >=6 && $idade <= 12){
-    
-        for($i =0; $i <= count($categorias); $i++){
-            if($categorias[$i] == 'infantil')
-                echo "O nadador ".$nome." compete na categoria ". $categorias[$i]; 
-        }        
-        
-    }
-    else if($idade >= 13 && $idade <= 18){
-         for($i =0; $i <= count($categorias); $i++){
-            if($categorias[$i] == 'adolescente')
-                echo "O nadador ", $nome, " compete na categoria adolescente"; 
-        }   
-    }
-    else{
-         for($i =0; $i <= count($categorias); $i++){
-            if($categorias[$i] == 'adulto')
-                echo "O nadador ", $nome, " compete na categoria adulto"; 
-        }   
-    }
+    <form action="script.php" method="post">
+        <label for="name">Nome</label>
+        <input type="text" name="name" id="name">
+        <label for="idade">Idade</label>
+        <input type="text" name="idade" id="idade">
+        <input type="submit" value="Enviar">
+    </form>
+</body>
+</html>
---
+++ script.php
@@ -0,0 +1,61 @@
+<?php
+   $categorias = [];
+    $categorias[] = 'infantil';
+    $categorias[] = 'adolescente';
+    $categorias[] = 'adulto';
+    $categorias[] = 'idoso';
+
+    $nome = $_POST['name'];
+    $idade = $_POST['idade'];
+   
+    if(empty($nome)){
+        echo "nome não pode ser vazio";
+        return;
+    }
+
+    if(strlen($nome) < 3){
+        echo "o nome deve conter mais que 3 caracter";
+        return;
+    }
+    if(strlen($nome) > 40){
+        echo "nome digitado excede o maximo permitido";
+        return;
+    }
+
+    if(empty($idade)){
+        echo "por favor informe a idade do competidor";
+        return;
+    }
+    if(!is_numeric($idade)){
+        echo "informe a sua idade no formato numerico";
+        return;
+    }
+    if($idade < 6){
+        echo "infelizmente a criança é muito nova para competir";
+        return;
+    }
+    if($idade >= 100){
+        echo "Parabens por viver ate os 100 anos, mas você não poderá competir devido a sua idade";
+        return;
+    }
+
+    if($idade >=6 && $idade <= 12){
+    
+        for($i =0; $i <= count($categorias); $i++){
+            if($categorias[$i] == 'infantil')
+                echo "O nadador ".$nome." compete na categoria ". $categorias[$i]; 
+        }        
+        
+    }
+    else if($idade >= 13 && $idade <= 18){
+         for($i =0; $i <= count($categorias); $i++){
+            if($categorias[$i] == 'adolescente')
+                echo "O nadador ". $nome. " compete na categoria ". $categorias[$i]; 
+        }   
+    }
+    else{
+         for($i =0; $i <= count($categorias); $i++){
+            if($categorias[$i] == 'adulto')
+                echo "O nadador ". $nome. " compete na categoria ". $categorias[$i]; 
+        }   
+    }
